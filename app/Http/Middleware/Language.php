bb2308aac25b3a97e5b99c66184435a66323686d
Sc3ne <gokaysolman@hotmail.com>
2016-09-16 00:25:47 +0300

    first commit

---
+++ app/Http/Middleware/Language.php
@@ -0,0 +1,67 @@
+<?php namespace App\Http\Middleware;
+
+use Closure, Config, App, Redirect, Route; // ... and so on
+
+class Language {
+
+    public function __construct() {
+        //
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // Make sure current locale exists.
+        $locale = $request->segment(1);
+        $languages = $this->getLanguages();
+
+        if ( ! array_key_exists($locale, $languages)) {
+            
+            $segments = $request->segments();
+
+            $acceptLanguage = $this->getAcceptLanguage($request);
+            $defaultLanguage = ( in_array($acceptLanguage, array_keys($this->getLanguages())) ? $acceptLanguage : Config::get('app.fallback_locale') );
+
+            $segments = array_merge(array($defaultLanguage), $segments);
+
+            return Redirect::to(implode('/', $segments));
+        }
+
+        App::setLocale($locale);
+
+        $this->setRoutes();
+
+        return $next($request);
+
+    }
+
+    private function getAcceptLanguage($request) {
+
+        return mb_substr($request->server('HTTP_ACCEPT_LANGUAGE'), 0, 2);
+
+    }
+
+    private function getLanguages() {
+
+        return [
+            'en' => 'English',
+            'tr' => 'Türkçe'
+        ];
+
+    }
+
+    private function setRoutes() {
+
+        Route::group([ 'middleware' => [ 'web' ], 'prefix' => App::getLocale() ], function() {
+            include app_path('/Http/routes.php');
+        });
+
+    }
+
+}
